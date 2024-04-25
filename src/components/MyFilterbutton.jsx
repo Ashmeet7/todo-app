ec2bc4e4e9cdbfcc519d1abdf91c09a22e394d99
ashmeet <ashmeet>
2024-04-25 16:12:15 +0530

    fixed viewport for mobile devices

--- src/components/MyFilterbutton.jsx
+++ src/components/MyFilterbutton.jsx
@@ -11,17 +11,17 @@ const MyFilterbutton = () => {
   }
   
   return (
-    <div className="flex space-x-4 items-center">
+    <div className="flex lg:items-center sm:flex-row flex-col">
       <select 
       value={currentfilter} 
       onChange={(e) => handfilter(e.target.value)}
-      className="text-sm px-2 py-1 border rounded border-gray-300 bg-white focus:outline-none">
+      className="text-sm px-2 py-2 border rounded border-gray-300 bg-white focus:outline-none">
         <option value="ALL">ALL</option>
         <option value="COMPLETE">COMPLETE</option>
         <option value="INCOMPLETE">INCOMPLETE</option>
       </select>
 
-      <button onClick={() => dispatch(markallcompleted())} className="text-sm px-2 py-1 rounded bg-violet-500 text-white">Mark All Completed</button>
+      <button onClick={() => dispatch(markallcompleted())} className="text-sm ml-3 px-2 py-2 rounded bg-violet-500 text-white">Mark All Completed</button>
     </div>
   )
 }
